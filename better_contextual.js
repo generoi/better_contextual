b5e109e60fc6cf20a91ef9b19ecde3946316ee19
Oskar Schöldström <public@oxy.fi>
2014-01-28 13:46:59 +0100

    Fix nested contextual links due to update in drupal core

---
+++ better_contextual.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+  Drupal.behaviors.betterContextual = {
+    attach: function(context) {
+      // Quick fix which makes sure nested contextual link menus are shown when
+      // clicked. This has some bugs but admins dont care.
+      $(context).on('click', 'a.contextual-links-trigger', function() {
+        $(this)
+          .next('ul.contextual-links')
+            .stop(true, true).slideToggle(100)
+          .parent('div.contextual-links-wrapper')
+            .toggleClass('contextual-links-active');
+        return false;
+      });
+    }
+  };
+}(jQuery));
